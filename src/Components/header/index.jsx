fa1383a2c176cdc7b8a952b3e34c34b565db8011
mahi <singhthakurmohan46@gmail.com>
2023-01-29 09:25:16 +0530

    Signed-off-by: mahi <singhthakurmohan46@gmail.com>

---
+++ src/Components/header/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import logo from '../../img/logo.png'
+
+const Header = () => {
+  return (
+    <header className='center'>
+      <img src={logo} alt='' style={{height:150}} />
+    </header>
+  )
+}
+
+export default Header
